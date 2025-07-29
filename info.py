d671e16bc79260290529ee3b464c6b5ca69259a1
Aditya38287 <adityapatel38287@gmail.com>
2025-07-29 16:46:08 +0530

    Update info.py

--- info.py
+++ info.py
@@ -88,7 +88,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/moviemagicchat_24')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+ADO9sWnqIxw1ZDhl')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/moviemagic_24')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'adityahelp24_bot') # Support Chat Link Without https:// or @
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/aditya2463')
@@ -96,7 +96,7 @@ OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/aditya2463')
 # True Or False
 AI_SPELL_CHECK = bool(environ.get('AI_SPELL_CHECK', True))
 PM_SEARCH = bool(environ.get('PM_SEARCH', True))
-BUTTON_MODE = bool(environ.get('BUTTON_MODE', True))
+BUTTON_MODE = bool(environ.get('BUTTON_MODE', False))
 MAX_BTN = bool(environ.get('MAX_BTN', True))
 IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', False))
 IMDB = bool(environ.get('IMDB', False))
